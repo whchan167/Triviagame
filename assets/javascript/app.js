9c82855d0675c669f27edb10fdf1e663ed256a00
wen <wen@wens-mbp.attlocal.net>
2016-11-16 01:57:29 -0600

    Triviagame

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-//creat and object obtains questions and array of answers.
+//create and object obtains questions and array of answers.
 var questions = ["Besides Tom Cruise, who is the only actor appear in all five 'Mission:Impossible' movies?",
 				 "In which time period do 'The Lord of the Rings' and 'The hobbits' take place?",
 				 "In the movie 'Dr. Strange', what does Stephen Strange do to defend Earth from Dormammu?",
@@ -14,15 +14,35 @@ var AnswerOption = [ ["Jeremy Renner", "Ving Rhames", "Simon Pegg", "Daniel Crai
    					 ["Gugu Mbatha", "Lily James", "Emma Watson", "Chloe Moretz"],
 					 ["10", "11", "12", "13"]
 					];	
-var quest = [];
-//creat an array for correct answer:
-var corrAns = ["B","C","C","A","C","C"]
+
+//create an array for correct answer:
+var corrAns = ["B","C","C","A","C","C"];
+
+//create the same array with different variables to push content back to empty array later
+var questions2=["Besides Tom Cruise, who is the only actor appear in all five 'Mission:Impossible' movies?",
+				 "In which time period do 'The Lord of the Rings' and 'The hobbits' take place?",
+				 "In the movie 'Dr. Strange', what does Stephen Strange do to defend Earth from Dormammu?",
+				 "In 'Fast and Furious 7', who plays a 'bad guy' role and injure the Rock?",
+				 "Who will play 'Belle' in the latest 'Beauty and the Beast' movie?",
+				 "How many total superheros show up at the airport fight scene in the 'Captain America: Civil War'"
+				]; 
+var AnswerOption2=[ ["Jeremy Renner", "Ving Rhames", "Simon Pegg", "Daniel Craig"],
+					 ["Forth Age", "Second Age", "Third Age", "Sixth Age"],
+					 ["Calling all Avengers", "Using ultimate weapon", "Creating infinite time loop", "Using mind control"], 
+					 ["Jason Statham", "Van Diesel", "Tyrese Gibson", "Paul Walker"],	
+   					 ["Gugu Mbatha", "Lily James", "Emma Watson", "Chloe Moretz"],
+					 ["10", "11", "12", "13"]
+					];
+var corrAns2=["B","C","C","A","C","C"];
 
 //set a variable for the timer counting down from 60s: 
 var time = 30;
 
 //activate the timer:
 timer();
+
+$("#hiddenBtn").hide();
+
 
 //create variable for number of correct or incorrect:
 var correct = 0;
@@ -38,6 +58,8 @@ $("#A").html(AnswerOption[ran][0]);
 $("#B").html(AnswerOption[ran][1]);
 $("#C").html(AnswerOption[ran][2]);
 $("#D").html(AnswerOption[ran][3]);
+
+
 
 //create the timer counting down from 60s
 function timer() {
@@ -58,31 +80,66 @@ function decrement(){
 		setTimeout(reset, 1000);
 		questions.splice(ran, 1);
 		AnswerOption.splice(ran, 1);
-		corrAns.splice(ran,1);
-	};
+		corrAns.splice(ran, 1);
+		};
+};
+function resetgame(){
+		timer();
+		correct=0;	
+		incorrect=0;
+		$("#correct").html("correct:" + correct);
+		$("#incorrect").html("incorrect:" + incorrect);
+		
+		ran = Math.floor(Math.random()*questions.length);
+
+		$("#quest").html(questions[ran]);
+		$("#A").html(AnswerOption[ran][0]);
+		$("#B").html(AnswerOption[ran][1]);
+		$("#C").html(AnswerOption[ran][2]);
+		$("#D").html(AnswerOption[ran][3]);
+
+		$("#timeclock").show();
+		$("#rightwrong").show();
+		$("#quest").show();
+		$("#button").show();
+		$("#hiddenBtn").hide();
+		$("#question").empty();
+
 };
 
-//creat reset for the game on the situation when the incorrect answer was entered or time up!
+//create reset for the game on the situation when the incorrect answer was entered or time up!
 function reset() {
-	//delete the question and answers so they will not appear again
 	questions.splice(ran, 1);
 	AnswerOption.splice(ran, 1);
-	corrAns.splice(ran,1);
+	corrAns.splice(ran, 1);
 
 	ran = Math.floor(Math.random()*questions.length);
 
 	if (questions.length===0){
 		stop();
-		$("#timeclock").empty();
-		$("#rightwrong").empty();
-		$("#qsection").html("<div><strong>Fantastic Job!</strong></div>"
+		$("#timeclock").hide();
+		$("#rightwrong").hide();
+		$("#quest").hide();
+		$("#button").hide();
+		$("#question").append("<div><strong>Fantastic Job!</strong></div>"
 						+"<div>Your result is:</div>"
 						+"<div>Number of correct answer:"+correct+"</div>"
 						+"<div>Number of incorrect answer:"+incorrect+"</div");
+		$("#hiddenBtn").show();
+		for (var i=0; i<questions2.length; i++){
+			questions.push(questions2[i]);
+			AnswerOption.push(AnswerOption2[i]);
+			corrAns.push(corrAns2[i]);
+			}
+		$("#hiddenBtn").on("click", function(){
+				resetgame();
+		});
+		console.log(corrAns);
+		console.log(questions);
+		console.log(AnswerOption);
 		return;
-	}
+	};
 
-	console.log(AnswerOption)
 
 	$("#quest").html(questions[ran]);
 	$("#A").html(AnswerOption[ran][0]);
@@ -91,16 +148,15 @@ function reset() {
 	$("#D").html(AnswerOption[ran][3]);
 	console.log(ran)
 	
-};
+};	
 
 //on click button to choose the answer
 	$("#A").on("click",function(){
 		if (corrAns[ran]=="A") {
 			correct++;
-			alert ("correct!");
 			$("#correct").html("correct:" + correct);
-			
-			setTimeout(reset, 1000)
+			alert ("You are correct!");
+			setTimeout(reset(), 1000)
 			time =30;
 		}
 		else { 
@@ -116,10 +172,9 @@ function reset() {
 	$("#B").on("click",function(){
 		if (corrAns[ran]=="B") {
 			correct++;
-			alert ("correct!");
 			$("#correct").html("correct:" + correct);
-			
-			setTimeout(reset, 1000)
+			alert ("You are correct!");
+			setTimeout(reset(), 1000)
 			time =30;
 
 		}
@@ -136,10 +191,9 @@ function reset() {
 	$("#C").on("click",function(){
 		if (corrAns[ran]=="C") {
 			correct++;
-			alert ("correct!");
 			$("#correct").html("correct:" + correct);
-			
-			setTimeout(reset, 1000);
+			alert ("You are correct!");
+			setTimeout(reset(), 1000)
 			time =30;
 
 		}
@@ -156,10 +210,9 @@ function reset() {
 	$("#D").on("click",function(){
 		if (corrAns[ran]=="D") {
 			correct++;
-			alert ("correct!");
 			$("#correct").html("correct:" + correct);
-			
-			setTimeout(reset, 1000);
+			alert ("You are correct!");
+			setTimeout(reset(), 1000)
 			time =30;
 
 		}
@@ -172,5 +225,5 @@ function reset() {
 
 		}
 	});
-
 });
+
